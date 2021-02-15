831138383d28ff46d54e5eab47268917f042f767
Mark Lodato <lodato@google.com>
2021-02-14 20:32:37 -0500

    Reformat with yapf.

--- mssb_1099b_to_txf.py
+++ mssb_1099b_to_txf.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 """mssb_1099b_to_txf converts simple Morgan Stanley (MSSB) 1099-B PDFs to TXF files."""
 
 import datetime
@@ -9,13 +8,14 @@ import sys
 
 if len(sys.argv) != 2:
     sys.exit(f'Usage: {sys.argv[0]} path-to-1099b-pdf')
-text = subprocess.check_output(['pdftotext', '-raw', sys.argv[1], '-']).decode()
+text = subprocess.check_output(['pdftotext', '-raw', sys.argv[1],
+                                '-']).decode()
 
 # Codes and structure are defined at
 # https://www.taxdataexchange.org/txf/txf-spec.html
 categories = {
-        'Short Term – Noncovered Securities': '711',
-        'Long Term – Noncovered Securities': '713',
+    'Short Term – Noncovered Securities': '711',
+    'Long Term – Noncovered Securities': '713',
 }
 
 # Match a section of sales for one sales category.
@@ -23,10 +23,9 @@ categories = {
 # 'Total Short Term Noncovered Securities' (without the hypen) so match
 # only on "^Total".
 categories_pattern = '|'.join(categories)
-section_expr = re.compile(
-        r'('+categories_pattern+r')'
-        r'(.*?)'
-        r'^Total', re.DOTALL|re.MULTILINE)
+section_expr = re.compile(r'(' + categories_pattern + r')'
+                          r'(.*?)'
+                          r'^Total', re.DOTALL | re.MULTILINE)
 
 # Fields: RefNumber Description CUSIP Quantity DateAcquired DateSold
 #         GrossProceeds CostBasis
@@ -41,13 +40,14 @@ section_expr = re.compile(
 #   12345A678
 #   1.000000 VARIOUS 02/01/20 $2,000.00 $1,9999.00
 row_expr = re.compile(
-        r'^(?P<descr>(\w| )+)\s+'
-        r'(?P<cusip>\w+)\s+'
-        r'(?P<quantity>\d+\.\d+)\s+'
-        r'(?P<acquired>(\d+/\d+/\d+|\w+))\s+'
-        r'(?P<sold>\d+/\d+/\d+)\s+'
-        r'(?P<proceeds>\$[0-9,.]+)\s+'
-        r'(?P<cost>\$[0-9,.]+)\s', re.DOTALL|re.MULTILINE)
+    r'^(?P<descr>(\w| )+)\s+'
+    r'(?P<cusip>\w+)\s+'
+    r'(?P<quantity>\d+\.\d+)\s+'
+    r'(?P<acquired>(\d+/\d+/\d+|\w+))\s+'
+    r'(?P<sold>\d+/\d+/\d+)\s+'
+    r'(?P<proceeds>\$[0-9,.]+)\s+'
+    r'(?P<cost>\$[0-9,.]+)\s', re.DOTALL | re.MULTILINE)
+
 
 def parseAndPrintRows(text, entry_code):
     for match in row_expr.finditer(text):
@@ -61,8 +61,9 @@ def parseAndPrintRows(text, entry_code):
         # These have a leading dollar sign.
         print(match.group('cost'))
         print(match.group('proceeds'))
-        print("$") # Wash sale. Leaving blank. They aren't handled here.
+        print("$")  # Wash sale. Leaving blank. They aren't handled here.
         print('^')
+
 
 print('V042')
 print('A mssb_1099b_to_txf')
@@ -72,4 +73,3 @@ for section_match in section_expr.finditer(text):
     entry_code = categories[section_match.group(1)]
     contents = section_match.group(2)
     parseAndPrintRows(contents, entry_code)
-
