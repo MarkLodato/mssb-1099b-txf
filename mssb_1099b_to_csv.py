63f97188d4b55ea3fc818910e43ca0eda046cfbf
Mark Lodato <lodato@google.com>
2021-02-14 19:01:42 -0500

    Factor out functionality into mssb_1099b_to_csv.
    This way users can convert to CSV if desired.

---
+++ mssb_1099b_to_csv.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+"""Converts Morgan Stanley (MSSB) 1099-B PDFs to CSV."""
+
+import collections
+import csv
+import datetime
+import re
+import subprocess
+import sys
+
+# Maps PDF category names to TXF codes.
+# See the following for codes and structure:
+# https://www.taxdataexchange.org/txf/txf-spec.html
+CATEGORIES = {
+    'Short Term – Noncovered Securities': '711',
+    'Long Term – Noncovered Securities': '713',
+}
+
+# Match a section of sales for one sales category.
+# The last line can say 'Total Short Term – Noncovered Securities' or
+# 'Total Short Term Noncovered Securities' (without the hypen) so match
+# only on "^Total".
+CATEGORIES_PATTERN = '|'.join(CATEGORIES)
+SECTION_EXPR = re.compile(r'(' + CATEGORIES_PATTERN + r')'
+                          r'(.*?)'
+                          r'^Total', re.DOTALL | re.MULTILINE)
+
+# Matches a row.
+#
+# Example:
+#   ALPHABET INC CL C
+#   12345A678
+#   1.000000 01/01/20 02/01/20 $2,000.00 $1,9999.00
+#
+# Example:
+#   1234 R18 ALPHABET INC CL C
+#   12345A678
+#   1.000000 VARIOUS 02/01/20 $2,000.00 $1,9999.00
+ROW_EXPR = re.compile(
+    '^'
+    r'(?:(\d+)\s+)?'  # RefNumber (optional)
+    r'(?:([a-zA-Z]\d\d)\s+)?'  # PlanNumber (optional)
+    r'([\w ]+)\s+'  # Description
+    r'(\w+)\s+'  # CUSIP
+    r'(\d+\.\d+)\s+'  # Quantity
+    r'((?:\d+/\d+/\d+|\w+))\s+'  # DateAcquired
+    r'(\d+/\d+/\d+)\s+'  # DateSold
+    r'(\$[0-9,.]+)\s+'  # GrossProceeds
+    r'(\$[0-9,.]+)\s',  # CostBasis
+    re.MULTILINE)
+
+# Fields of the output, suitable as a CSV header.
+FIELDS = [
+    'Category', 'RefNumber', 'PlanNumber', 'Description', 'CUSIP', 'Quantity',
+    'DateAcquired', 'DateSold', 'GrossProceeds', 'CostBasis'
+]
+
+# Type of each row of output.
+Row = collections.namedtuple('Row', FIELDS)
+
+
+def _parseSection(section_text, section_name):
+    return [
+        Row(*((section_name, ) + row_match.groups()))
+        for row_match in ROW_EXPR.finditer(section_text)
+    ]
+
+
+def parsePdf(path):
+    """Returns a list of `Row` tuples."""
+    text = subprocess.check_output(['pdftotext', '-raw', path, '-']).decode()
+    return [
+        row  #
+        for section_match in SECTION_EXPR.finditer(text) for row in
+        _parseSection(section_match.group(2), section_match.group(1))
+    ]
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.exit(f'Usage: {sys.argv[0]} <mssb_1099b_file.pdf>')
+    rows = parsePdf(sys.argv[1])
+    writer = csv.writer(sys.stdout)
+    writer.writerow(FIELDS)
+    writer.writerows(rows)
--- mssb_1099b_to_txf.py
+++ mssb_1099b_to_txf.py
@@ -6,70 +6,41 @@ import re
 import subprocess
 import sys
 
-if len(sys.argv) != 2:
-    sys.exit(f'Usage: {sys.argv[0]} path-to-1099b-pdf')
-text = subprocess.check_output(['pdftotext', '-raw', sys.argv[1],
-                                '-']).decode()
-
-# Codes and structure are defined at
-# https://www.taxdataexchange.org/txf/txf-spec.html
-CATEGORIES = {
-    'Short Term – Noncovered Securities': '711',
-    'Long Term – Noncovered Securities': '713',
-}
-
-# Match a section of sales for one sales category.
-# The last line can say 'Total Short Term – Noncovered Securities' or
-# 'Total Short Term Noncovered Securities' (without the hypen) so match
-# only on "^Total".
-CATEGORIES_PATTERN = '|'.join(CATEGORIES)
-SECTION_EXPR = re.compile(r'(' + CATEGORIES_PATTERN + r')'
-                          r'(.*?)'
-                          r'^Total', re.DOTALL | re.MULTILINE)
-
-# Fields: RefNumber Description CUSIP Quantity DateAcquired DateSold
-#         GrossProceeds CostBasis
-#
-# Example:
-#   1234 ALPHABET INC CL C
-#   12345A678
-#   1.000000 01/01/20 02/01/20 $2,000.00 $1,9999.00
-#
-# Example:
-#   1234 ALPHABET INC CL C
-#   12345A678
-#   1.000000 VARIOUS 02/01/20 $2,000.00 $1,9999.00
-ROW_EXPR = re.compile(
-    r'^(?P<descr>(\w| )+)\s+'
-    r'(?P<cusip>\w+)\s+'
-    r'(?P<quantity>\d+\.\d+)\s+'
-    r'(?P<acquired>(\d+/\d+/\d+|\w+))\s+'
-    r'(?P<sold>\d+/\d+/\d+)\s+'
-    r'(?P<proceeds>\$[0-9,.]+)\s+'
-    r'(?P<cost>\$[0-9,.]+)\s', re.DOTALL | re.MULTILINE)
+import mssb_1099b_to_csv
 
 
-def parseAndPrintRows(text, entry_code):
-    for match in ROW_EXPR.finditer(text):
-        print('TD')
-        print('N' + entry_code)
-        print('C1')
-        print('L1')
-        print('P' + match.group('descr'))
-        print('D' + match.group('acquired'))
-        print('D' + match.group('sold'))
-        # These have a leading dollar sign.
-        print(match.group('cost'))
-        print(match.group('proceeds'))
-        print("$")  # Wash sale. Leaving blank. They aren't handled here.
-        print('^')
+def parseAndPrintRow(row):
+    entry_code = mssb_1099b_to_csv.CATEGORIES[row.Category]
+    description = ' '.join(filter(None, [
+        row.RefNumber,
+        row.PlanNumber,
+        row.Description,
+    ]))
+    print('TD')
+    print('N' + entry_code)
+    print('C1')
+    print('L1')
+    print('P' + description)
+    print('D' + row.DateAcquired)
+    print('D' + row.DateSold)
+    # These have a leading dollar sign.
+    print(row.CostBasis)
+    print(row.GrossProceeds)
+    print("$")  # Wash sale. Leaving blank. They aren't handled here.
+    print('^')
 
 
-print('V042')
-print('A mssb_1099b_to_txf')
-print('D ' + datetime.datetime.now().strftime('%m/%d/%Y'))
-print('^')
-for section_match in SECTION_EXPR.finditer(text):
-    entry_code = CATEGORIES[section_match.group(1)]
-    contents = section_match.group(2)
-    parseAndPrintRows(contents, entry_code)
+def parseAndPrintPdf(path):
+    rows = mssb_1099b_to_csv.parsePdf(path)
+    print('V042')
+    print('A mssb_1099b_to_txf')
+    print('D ' + datetime.datetime.now().strftime('%m/%d/%Y'))
+    print('^')
+    for row in rows:
+        parseAndPrintRow(row)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.exit(f'Usage: {sys.argv[0]} path-to-1099b-pdf')
+    parseAndPrintPdf(sys.argv[1])
